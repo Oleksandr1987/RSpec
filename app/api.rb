8f391ae74b6ec4e05cd655394ad4c0b45f9e45b8
Oleksandr1987 <solovij1987@gmail.com>
2022-08-13 22:03:23 +0300

    Handing Success and Refactoring

--- app/api.rb
+++ app/api.rb
@@ -4,11 +4,21 @@ require 'json'
 module ExpenseTracker
   class API < Sinatra::Base
     post '/expenses' do 
-      JSON.generate('expense_id' => 42)
+        status 404
+        
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)  
+      JSON.generate('expense_id' => result.expense_id)
     end
       
     get '/expenses/:date' do
         JSON.generate([])  
     end
+
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
   end
 end
